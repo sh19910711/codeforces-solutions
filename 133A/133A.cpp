2516701997bcc55c67272de67c0794e57eb4c509
Hiroyuki Sano <sh19910711@gmail.com>
2013-04-08 09:01:35 +0900

    Codeforces 133A: Accepted

---
+++ 133A/133A.cpp
@@ -0,0 +1,87 @@
+
+// @snippet<sh19910711/contest:headers.cpp>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <complex>
+#include <functional>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+// @snippet<sh19910711/contest:solution/interface.cpp>
+namespace solution {
+    class ISolution {
+    public:
+        virtual void init() {};
+        virtual bool input() { return false; };
+        virtual void output() {};
+        virtual int run() = 0;
+    };
+}
+
+// @snippet<sh19910711/contest:solution/typedef.cpp>
+namespace solution {
+    typedef std::istringstream ISS;
+    typedef std::ostringstream OSS;
+    typedef std::vector<std::string> VS;
+    typedef long long LL;
+    typedef int INT;
+    typedef std::vector<INT> VI;
+    typedef std::vector<VI> VVI;
+    typedef std::pair<INT,INT> II;
+    typedef std::vector<II> VII;
+}
+
+// @snippet<sh19910711/contest:solution/solution.cpp>
+namespace solution {
+    using namespace std;
+
+    const string YES = "YES";
+    const string NO = "NO";
+    string s;
+    
+    class Solution: public ISolution {
+    public:
+        
+        bool input() {
+            return cin >> s;
+        }
+        
+        string solve() {
+            bool f1 = count( s.begin(), s.end(), 'H' ) > 0;
+            bool f2 = count( s.begin(), s.end(), 'Q' ) > 0;
+            bool f3 = count( s.begin(), s.end(), '9' ) > 0;
+            return ( ( f1 | f2 | f3 ) ? YES : NO );
+        }
+
+        void output( string s ) {
+            cout << s << endl;
+        }
+        
+        int run() {
+            while ( init(), input() ) {
+                output(solve());
+            }
+            return 0;
+        }
+        
+    };
+}
+
+// @snippet<sh19910711/contest:main.cpp>
+int main() {
+    return solution::Solution().run();
+}
+
