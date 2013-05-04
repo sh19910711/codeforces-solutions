583d9b2314edf8b47ef74ea42f2945b6b0ea2213
Hiroyuki Sano <sh19910711@gmail.com>
2013-05-04 18:40:04 +0900

    Codeforces 71A: import方法を修正した

--- 71A/71A.go
+++ 71A/71A.go
@@ -1,22 +1,22 @@
 package main
 
-import "fmt"
+import . "fmt"
 
 func get_string( s string ) string {
     n := len(s)
     if n > 10 {
-        return fmt.Sprintf("%c%s%c", s[0], fmt.Sprintf("%d", n - 2), s[n - 1])
+        return Sprintf("%c%s%c", s[0], Sprintf("%d", n - 2), s[n - 1])
     }
     return s
 }
 
 func main() {
     var n int
-    fmt.Scanf("%d\n", &n)
+    Scanf("%d\n", &n)
     for i := 0; i < n; i ++ {
         var s string
-        fmt.Scanf("%s\n", &s)
-        fmt.Printf("%s\n", get_string(s))
+        Scanf("%s\n", &s)
+        Printf("%s\n", get_string(s))
     }
 }
 
