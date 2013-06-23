8593c57f1726b9d08d3c4d764ff492d1410ac713
Hiroyuki Sano <sh19910711@gmail.com>
2013-06-23 23:11:45 +0900

    Codeforces 320C: Pretest Passed => Accepted

---
+++ 320C/320C.cpp
@@ -0,0 +1,152 @@
+
+// @snippet<sh19910711/contest:headers.cpp>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <complex>
+#include <functional>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+// @snippet<sh19910711/contest:solution/interface.cpp>
+namespace solution {
+  class ISolution {
+  public:
+    virtual int run() = 0;
+    
+  protected:
+    virtual bool action() = 0;
+    virtual void init() {};
+    virtual bool input() { return false; };
+    virtual void output() {};
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution-base.cpp>
+namespace solution {
+  class SolutionBase: public ISolution {
+  public:
+    virtual int run() {
+      while ( action() );
+      return 0;
+    }
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/typedef.cpp>
+namespace solution {
+  typedef std::istringstream ISS;
+  typedef std::ostringstream OSS;
+  typedef std::vector<std::string> VS;
+  typedef long long LL;
+  typedef int INT;
+  typedef std::vector<INT> VI;
+  typedef std::vector<VI> VVI;
+  typedef std::pair<INT,INT> II;
+  typedef std::vector<II> VII;
+}
+
+// @snippet<sh19910711/contest:solution/namespace-area.cpp>
+namespace solution {
+  // namespaces, types
+  using namespace std;
+  
+}
+
+// @snippet<sh19910711/contest:solution/variables-area.cpp>
+namespace solution {
+  // constant vars
+  const LL MOD = 1000000007LL;
+  const int SIZE = 100 + 11;
+  
+  // storages
+  string x;
+  LL result;
+  LL P[SIZE];
+}
+
+// @snippet<sh19910711/contest:math/mod_pow.cpp>
+namespace math {
+  template<class T> T mod_pow( T x, T n, T mod ) {
+    if ( n == 0 ) return 1;
+    T res = mod_pow( x * x % mod, n / 2, mod );
+    if ( n & 1 ) res = res * x % mod;
+    return res;
+  }
+}
+
+// @snippet<sh19910711/contest:solution/solver-area.cpp>
+namespace solution {
+  class Solver {
+  public:
+    void solve() {
+      reverse(x.begin(), x.end());
+
+      int n = x.size();
+      LL res = 0;
+      LL diff = math::mod_pow(2LL, (LL)(n - 1), MOD);
+
+      for ( int i = 0; i < n; ++ i )
+        P[i] = math::mod_pow(2LL, (LL)i, MOD);
+
+      for ( int i = 0; i < n; ++ i ) {
+        if ( x[i] == '1' )
+          res = ( res + ( diff * P[i] ) % MOD ) % MOD;
+      }
+      result = res;
+    }
+    
+  private:
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution.cpp>
+namespace solution {
+  class Solution: public SolutionBase {
+  public:
+  protected:
+    virtual bool action() {
+      init();
+      if ( ! input() )
+        return false;
+      solver.solve();
+      output();
+      return true;
+    }
+    
+    bool input() {
+      return cin >> x;
+    }
+
+    void output() {
+      cout << result << endl;
+    }
+    
+  private:
+    Solver solver;
+    
+  };
+}
+
+// @snippet<sh19910711/contest:main.cpp>
+int main() {
+  return solution::Solution().run();
+}
+
+
+
