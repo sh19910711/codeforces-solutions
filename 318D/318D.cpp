06cbc87b6147df1cec19968132e12c1e8ad6b821
Hiroyuki Sano <sh19910711@gmail.com>
2013-07-12 17:17:12 +0900

    Codeforces 318D: Accepted

--- 318D/318D.cpp
+++ 318D/318D.cpp
@@ -20,96 +20,155 @@
 #include <cstring>
 #include <cmath>
 
-// @snippet<sh19910711/contest:solution/typedef.cpp>
-namespace solution {
-    typedef std::istringstream ISS;
-    typedef std::ostringstream OSS;
-    typedef std::vector<std::string> VS;
-    typedef long long LL;
-    typedef int INT;
-    typedef std::vector<INT> VI;
-    typedef std::vector<VI> VVI;
-    typedef std::pair<INT,INT> II;
-    typedef std::vector<II> VII;
-}
-
 // @snippet<sh19910711/contest:solution/interface.cpp>
 namespace solution {
-    class ISolution {
-    public:
-        virtual void init() {};
-        virtual bool input() { return false; };
-        virtual void output() {};
-        virtual int run() = 0;
-    };
+  class ISolution {
+  public:
+    virtual int run() = 0;
+    
+  protected:
+    virtual bool action() = 0;
+    virtual void init() {};
+    virtual bool input() { return false; };
+    virtual void output() {};
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution-base.cpp>
+namespace solution {
+  class SolutionBase: public ISolution {
+  public:
+    virtual int run() {
+      while ( action() );
+      return 0;
+    }
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/typedef.cpp>
+namespace solution {
+  typedef std::istringstream ISS;
+  typedef std::ostringstream OSS;
+  typedef std::vector<std::string> VS;
+  typedef long long LL;
+  typedef int INT;
+  typedef std::vector<INT> VI;
+  typedef std::vector<VI> VVI;
+  typedef std::pair<INT,INT> II;
+  typedef std::vector<II> VII;
+}
+
+// @snippet<sh19910711/contest:solution/namespace-area.cpp>
+namespace solution {
+  // namespaces, types
+  using namespace std;
+  typedef map <II, int> Map;
+}
+
+// @snippet<sh19910711/contest:solution/variables-area.cpp>
+namespace solution {
+  // constant vars
+  const int SIZE = 50000 + 11;
+  const int dx[4] = {1, -1, 0 ,0};
+  const int dy[4] = {0, 0, 1, -1};
+  const int GRID_SIZE = 400;
+  const int GRID_OFFSET = GRID_SIZE / 2;
+
+  // storages
+  int n, t;
+  int X[SIZE];
+  int Y[SIZE];
+  int CNT[GRID_SIZE * GRID_SIZE];
+
+  int results;
+  int A[SIZE];
+}
+
+// @snippet<sh19910711/contest:solution/solver-area.cpp>
+namespace solution {
+  class Solver {
+  public:
+    void solve() {
+      for ( int i = 0; i < n; ++ i )
+        rec(0, 0);
+
+      results = 0;
+      for ( int i = 0; i < t; ++ i ) {
+        A[results ++] = query(X[i], Y[i]);
+      }
+    }
+
+    int& mapping( int x, int y ) {
+      x += GRID_OFFSET;
+      y += GRID_OFFSET;
+      return CNT[y * GRID_OFFSET + x];
+    }
+
+    void rec( int x, int y ) {
+      int& cnt = mapping(x, y);
+      cnt ++;
+      if ( cnt >= 4 ) {
+        cnt -= 4;
+        for ( int k = 0; k < 4; ++ k ) {
+          int nx = x + dx[k];
+          int ny = y + dy[k];
+          rec(nx, ny);
+        }
+      }
+    }
+
+    int query( int x, int y ) {
+      if ( abs(x) >= GRID_OFFSET || abs(y) >= GRID_OFFSET )
+        return 0;
+      return mapping(x, y);
+    }
+    
+  private:
+    
+  };
 }
 
 // @snippet<sh19910711/contest:solution/solution.cpp>
 namespace solution {
-    using namespace std;
-
-    const int dr[4] = {0, 0, 1, -1};
-    const int dc[4] = {1, -1, 0, 0};
-    int n, t;
-    int M[1000][1000];
-
-    int query(int x, int y) {
-        return M[x][y];
+  class Solution: public SolutionBase {
+  public:
+  protected:
+    virtual bool action() {
+      init();
+      if ( ! input() )
+        return false;
+      solver.solve();
+      output();
+      return true;
     }
 
-    void rec( int x, int y ) {
-        if ( x < 0 || y < 0 || x >= 1000 || y >= 1000 )
-            return;
-        if ( M[x][y] < 3 ) {
-            M[x][y] ++;
-            return;
-        }
-        M[x][y] = 0;
-        for ( int i = 0; i < 4; ++ i ) {
-            rec(x + dr[i], y + dc[i]);
-        }
+    void init() {
+      fill(CNT, CNT + GRID_SIZE * GRID_SIZE, 0);
     }
 
-    void solve() {
-        for ( int i = 0; i < n; ++ i )
-            rec(500, 500);
-        for ( int i = 0; i < t; ++ i ) {
-            int x, y;
-            cin >> x >> y;
-            x += 500;
-            y += 500;
-            if ( x < 0 || y < 0 || x >= 1000 || y >= 1000 )
-                cout << query(x, y) << endl;
-            else
-                cout << 0 << endl;
-        }
+    bool input() {
+      if ( ! ( cin >> n >> t ) )
+        return false;
+      for ( int i = 0; i < t; ++ i )
+        cin >> X[i] >> Y[i];
+      return true;
+    }
+
+    void output() {
+      for ( int i = 0; i < results; ++ i )
+        cout << A[i] << endl;
     }
     
-    class Solution: public ISolution {
-    public:
-        void init() {
-            for ( int i = 0; i < 1000; ++ i )
-                for ( int j = 0; j < 1000; ++ j )
-                    M[i][j] = 0;
-        }
-        
-        bool input() {
-            return cin >> n >> t;
-        }
-        
-        int run() {
-            while ( init(), input() ) {
-                solve();
-                output();
-            }
-            return 0;
-        }
-        
-    };
+  private:
+    Solver solver;
+    
+  };
 }
 
 // @snippet<sh19910711/contest:main.cpp>
 int main() {
-    return solution::Solution().run();
+  return solution::Solution().run();
 }
 
