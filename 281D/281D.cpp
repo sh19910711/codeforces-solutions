103759374d5a28f9a5e8bc20960c17c7392be21d
Hiroyuki Sano <sh19910711@gmail.com>
2013-07-16 19:43:59 +0900

    Codeforces 281D: Accepted

--- 281D/281D.cpp
+++ 281D/281D.cpp
@@ -22,115 +22,172 @@
 
 // @snippet<sh19910711/contest:solution/interface.cpp>
 namespace solution {
-    class ISolution {
-    public:
-        virtual void init() {};
-        virtual bool input() { return false; };
-        virtual void output() {};
-        virtual int run() = 0;
-    };
+  class SolutionInterface {
+  public:
+    virtual int run() = 0;
+    
+  protected:
+    virtual void pre_calc() {}
+    virtual bool action() = 0;
+    virtual void init() {};
+    virtual bool input() { return false; };
+    virtual void output() {};
+    
+    SolutionInterface() {}
+    
+  private:
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution-base.cpp>
+namespace solution {
+  class SolutionBase: public SolutionInterface {
+  public:
+    virtual int run() {
+      pre_calc();
+      while ( action() );
+      return 0;
+    }
+    
+  };
 }
 
 // @snippet<sh19910711/contest:solution/typedef.cpp>
 namespace solution {
-    typedef std::istringstream ISS;
-    typedef std::ostringstream OSS;
-    typedef std::vector<std::string> VS;
-    typedef long long LL;
-    typedef int INT;
-    typedef std::vector<INT> VI;
-    typedef std::vector<VI> VVI;
-    typedef std::pair<INT,INT> II;
-    typedef std::vector<II> VII;
+  typedef std::istringstream ISS;
+  typedef std::ostringstream OSS;
+  typedef std::vector<std::string> VS;
+  typedef long long LL;
+  typedef int INT;
+  typedef std::vector<INT> VI;
+  typedef std::vector<VI> VVI;
+  typedef std::pair<INT,INT> II;
+  typedef std::vector<II> VII;
 }
 
-// @snippet<sh19910711/contest:io/config.cpp>
+// @snippet<sh19910711/contest:io/pair.cpp>
 namespace io {
-    const std::string OUTPUT_VECTOR_DELIMITER = ",";
+  template <class A, class B> std::ostream& operator<<( std::ostream& os, const std::pair<A,B>& p ) {
+    return os << "(" << p.first << "," << p.second << ")";
+  }
+  template <class A, class B> std::istream& operator>>( std::istream& is, std::pair<A,B>& p ) {
+    return is >> p.first >> p.second;
+  }
 }
 
-// @snippet<sh19910711/contest:io/vector.cpp>
-namespace io {
-    template <typename T> std::ostream& operator <<( std::ostream& os, const std::vector<T>& v ) {
-        typedef typename std::vector<T>::const_iterator Iterator;
-        for ( Iterator it_i = v.begin(); it_i != v.end(); ++ it_i ) {
-            os << *it_i;
-            if ( it_i + 1 != v.end() )
-                os << OUTPUT_VECTOR_DELIMITER;
-        }
-        return os;
-    }
-    template <typename T> std::istream& operator >>( std::istream& is, std::vector<T>& v ) {
-        int n = v.size();
-        for ( int i = 0; i < n; ++ i ) {
-            is >> v[i];
-        }
-        return is;
-    }
+// @snippet<sh19910711/contest:solution/namespace-area.cpp>
+namespace solution {
+  // namespaces, types
+  using namespace std;
+  using namespace io;
+
+  typedef set<int> Set;
+  typedef set<int, greater<int> > SetRev;
 }
 
+// @snippet<sh19910711/contest:solution/variables-area.cpp>
+namespace solution {
+  // constant vars
+  const int SIZE = 100000 + 11;
+  // storages
+  int n;
+  int A[SIZE];
+  II S[SIZE];
+
+  Set P;
+  SetRev P_rev;
+
+  int result;
+}
+
+// @snippet<sh19910711/contest:solution/solver-area.cpp>
+namespace solution {
+  class Solver {
+  public:
+    void solve() {
+      normalize();
+      result = get_maximum_xor();
+    }
+
+    int get_maximum_xor() {
+      int res = 0;
+
+      for ( int i = 0; i < n; ++ i ) {
+        int current_value = S[i].first;
+        Set::iterator it_after = P.upper_bound(S[i].second);
+        Set::iterator it_before = P_rev.upper_bound(S[i].second);
+
+        if ( it_after != P.end() ) {
+          int after_value = A[*it_after];
+          res = max(res, current_value ^ after_value);
+        }
+
+        if ( it_before != P_rev.end() ) {
+          int before_value = A[*it_before];
+          res = max(res, current_value ^ before_value);
+        }
+
+        P.insert(S[i].second);
+        P_rev.insert(S[i].second);
+      }
+
+      return res;
+    }
+    
+    void normalize() {
+      for ( int i = 0; i < n; ++ i ) {
+        S[i] = II(A[i], i);
+      }
+      sort(S, S + n, greater<II>());
+    }
+    
+  private:
+    
+  };
+}
 
 // @snippet<sh19910711/contest:solution/solution.cpp>
 namespace solution {
-    using namespace std;
-    using namespace io;
+  class Solution: public SolutionBase {
+  public:
+  protected:
+    virtual bool action() {
+      init();
+      if ( ! input() )
+        return false;
+      solver.solve();
+      output();
+      return true;
+    }
+
+    void init() {
+      result = -1;
+      P.clear();
+      P_rev.clear();
+    }
     
-    const int SIZE = 100011;
-    const int NONE = -1;
-    int n;
-    int A[SIZE];
-    map <int, int> B;
-    int S[SIZE];
+    bool input() {
+      if ( ! ( cin >> n ) )
+        return false;
+      for ( int i = 0; i < n; ++ i )
+        cin >> A[i];
+      return true;
+    }
     
-    class Solution: public ISolution {
-    public:
-        void init() {
-            B.clear();
-        }
-        
-        bool input() {
-            if ( ! ( cin >> n && n ) )
-                return false;
-            for ( int i = 0; i < n; ++ i )
-                cin >> A[i];
-            return true;
-        }
-        
-        void solve() {
-            for ( int i = 0; i < n; ++ i ) {
-                B[A[i]] = i;
-            }
-            for ( int i = 0; i < n; ++ i )
-                S[i] = A[i];
-            sort( S, S+n );
-            
-            for ( int i = 0; i < n; ++ i ) {
-                int a = A[i];
-                int d = log2((double)a)+1;
-                for ( int j = d - 1; j >= 0; -- j ) {
-                    int bj = 1 << j;
-                    if ( ( a & bj ) == 0 ) {
-                        cout << a << " - " << j << endl;
-                    }
-                }
-            }
-        }
-        
-        int run() {
-            while ( init(), input() ) {
-                solve();
-                output();
-            }
-            return 0;
-        }
-        
-    };
+    void output() {
+      cout << result << endl;
+    }
+    
+  private:
+    Solver solver;
+    
+  };
 }
 
 // @snippet<sh19910711/contest:main.cpp>
 int main() {
-    return solution::Solution().run();
+  return solution::Solution().run();
 }
-
 
 
