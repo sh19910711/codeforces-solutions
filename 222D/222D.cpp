d8b4ee8c030282574f3b128753d88f969a2f8a0c
Hiroyuki Sano <sh19910711@gmail.com>
2013-07-23 19:35:36 +0900

    Codeforces 222D: Wrong Answer

---
+++ 222D/222D.cpp
@@ -0,0 +1,172 @@
+
+// @snippet<sh19910711/contest:headers.cpp>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <complex>
+#include <functional>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+// @snippet<sh19910711/contest:solution/interface.cpp>
+namespace solution {
+  class SolutionInterface {
+  public:
+    virtual int run() = 0;
+    
+  protected:
+    virtual void pre_calc() {}
+    virtual bool action() = 0;
+    virtual void init() {};
+    virtual bool input() { return false; };
+    virtual void output() {};
+    
+    SolutionInterface() {}
+    
+  private:
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution-base.cpp>
+namespace solution {
+  class SolutionBase: public SolutionInterface {
+  public:
+    virtual int run() {
+      pre_calc();
+      while ( action() );
+      return 0;
+    }
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/typedef.cpp>
+namespace solution {
+  typedef std::istringstream ISS;
+  typedef std::ostringstream OSS;
+  typedef std::vector<std::string> VS;
+  typedef long long LL;
+  typedef int INT;
+  typedef std::vector<INT> VI;
+  typedef std::vector<VI> VVI;
+  typedef std::pair<INT,INT> II;
+  typedef std::vector<II> VII;
+}
+
+// @snippet<sh19910711/contest:solution/namespace-area.cpp>
+namespace solution {
+  // namespaces, types
+  using namespace std;
+  
+}
+
+// @snippet<sh19910711/contest:solution/variables-area.cpp>
+namespace solution {
+  // constant vars
+  const int SIZE = 100000 + 11;
+  const int NONE = std::numeric_limits<int>::min();
+  // storages
+  int n, x;
+  int A[SIZE];
+  int B[SIZE];
+
+  int result;
+}
+
+// @snippet<sh19910711/contest:solution/solver-area.cpp>
+namespace solution {
+  class Solver {
+  public:
+    void solve() {
+      sort_list();
+      result = get_worst_rank();
+      for ( int i = 0; i < n; ++ i ) {
+        std::swap(A[i], B[i]);
+      }
+      result = max(result, get_worst_rank());
+    }
+
+    void sort_list() {
+      std::sort(A, A + n);
+      std::sort(B, B + n);
+    }
+
+    int get_worst_rank() {
+      int res = NONE;
+      int min_sum = std::numeric_limits<int>::max();
+      for ( int i = 0; i < n; ++ i ) {
+        int key = x - A[i];
+        if ( key >= 0 ) {
+          int id = std::lower_bound(B, B + n, key) - B;
+          if ( id >= n )
+            continue;
+          int sum = A[i] + B[id];
+          if ( sum >= min_sum )
+            continue;
+          min_sum = sum;
+          res = max(res, n - id);
+        }
+      }
+      return res;
+    }
+    
+  private:
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution.cpp>
+namespace solution {
+  class Solution: public SolutionBase {
+  public:
+  protected:
+    virtual bool action() {
+      init();
+      if ( ! input() )
+        return false;
+      solver.solve();
+      output();
+      return true;
+    }
+
+    bool input() {
+      if ( ! ( cin >> n >> x ) ) {
+        return false;
+      }
+      for ( int i = 0; i < n; ++ i ) {
+        cin >> A[i];
+      }
+      for ( int i = 0; i < n; ++ i ) {
+        cin >> B[i];
+      }
+      return true;
+    }
+
+    void output() {
+      cout << 1 << " " << result << endl;
+    }
+
+  private:
+    Solver solver;
+    
+  };
+}
+
+// @snippet<sh19910711/contest:main.cpp>
+int main() {
+  return solution::Solution().run();
+}
+
