5257d0407b33bb64e1b7d972b7402e345c9f60ea
Hiroyuki Sano <sh19910711@gmail.com>
2013-07-25 02:48:04 +0900

    Codeforces 332C: Unsubmitted

--- 332C/332C.cpp
+++ 332C/332C.cpp
@@ -69,18 +69,11 @@ namespace solution {
   typedef std::ostringstream OSS;
   typedef std::vector<std::string> VS;
   typedef long long LL;
-  typedef int INT;
+  typedef LL INT;
   typedef std::vector<INT> VI;
   typedef std::vector<VI> VVI;
   typedef std::pair<INT,INT> II;
   typedef std::vector<II> VII;
-}
-
-// @snippet<sh19910711/contest:solution/namespace-area.cpp>
-namespace solution {
-  // namespaces, types
-  using namespace std;
-  using namespace io;
 }
 
 // @snippet<sh19910711/contest:tree/interface_segment_tree.cpp>
@@ -96,13 +89,13 @@ namespace tree {
 // @snippet<sh19910711/contest:tree/rmq_tree.cpp>
 namespace tree {
   // RMQ Tree の実装
-  template <class T> class RMQTree: public tree::ISegmentTree<T> {
+  template <class T> class RMQTree_min: public tree::ISegmentTree<T> {
   public:
     T INF;
     int n;
     std::vector<T> D;
-    RMQTree(): INF( std::numeric_limits<T>::max() ) { n = 0; }
-    RMQTree( int n_ ): INF( std::numeric_limits<T>::max() ) {init(n_);}
+    RMQTree_min(): INF( std::numeric_limits<T>::max() ) { n = 0; }
+    RMQTree_min( int n_ ): INF( std::numeric_limits<T>::max() ) {init(n_);}
     void init( int n_ ) {
       n = 1;
       while ( n < n_ ) n *= 2;
@@ -128,12 +121,59 @@ namespace tree {
   };
 }
 
+// @snippet<sh19910711/contest:tree/rmq_tree.cpp>
+namespace tree {
+  // RMQ Tree の実装
+  template <class T> class RMQTree: public tree::ISegmentTree<T> {
+  public:
+    T INF;
+    int n;
+    std::vector<T> D;
+    RMQTree(): INF( std::numeric_limits<T>::min() ) { n = 0; }
+    RMQTree( int n_ ): INF( std::numeric_limits<T>::min() ) {init(n_);}
+    void init( int n_ ) {
+      n = 1;
+      while ( n < n_ ) n *= 2;
+      D.resize( 2 * n - 1 );
+      for ( int i = 0; i < 2 * n - 1; ++ i ) D[i] = INF;
+    }
+    int parent( const int k ) { return ( k - 1 ) / 2; }
+    int left( const int k ) { return 2 * k + 1; }
+    int right( const int k ) { return 2 * k + 2; }
+    // D[k] = a;
+    void update( int k, T a ) {
+      k += n - 1, D[k] = a;
+      while ( k > 0 ) k = parent(k), D[k] = std::max( D[left(k)], D[right(k)] );
+    }
+    T query( int a, int b, int k, int l, int r ) {
+      if ( r <= a || b <= l ) return INF;
+      if ( a <= l && r <= b ) return D[k];
+      T vl = query( a, b, left(k), l, ( l + r ) / 2 );
+      T vr = query( a, b, right(k), ( l + r ) / 2, r );
+      return std::max( vl, vr );
+    }
+    T query( int a, int b ) { return query( a, b, 0, 0, n ); }
+  };
+}
+
+// @snippet<sh19910711/contest:solution/namespace-area.cpp>
+namespace solution {
+  // namespaces, types
+  using namespace std;
+  using namespace io;
+
+  typedef II Entry; // (value, real-index, pos)
+}
+
 // @snippet<sh19910711/contest:solution/variables-area.cpp>
 namespace solution {
   // constant vars
   const int SIZE = 100000 + 11;
   const int SEG_TREE_DATA_SIZE = 2 * SIZE - 1 + 11;
   const LL IDENTITY = std::numeric_limits<LL>::min();
+  const LL IDENTITY_MIN = std::numeric_limits<LL>::max();
+  const Entry IDENTITY_ENTRY(IDENTITY, IDENTITY);
+  const Entry IDENTITY_ENTRY_MIN(IDENTITY_MIN, IDENTITY_MIN);
   
   // storages
   int n, p, k;
@@ -142,45 +182,13 @@ namespace solution {
   
   II C[SIZE]; // (A[i], i)
   II D[SIZE]; // (B[i], i)
+  tree::RMQTree<Entry> tree_a;
+  tree::RMQTree<Entry> tree_b;
+  int tree_pos_a[SIZE]; // A[i]がtreeの何番目にあるか
+  int tree_pos_b[SIZE]; // B[i]がtreeの
   
   int nice[SIZE];
   int nice_num;
-  
-  class SegmentTree {
-  public:
-    SegmentTree() {}
-    SegmentTree( int n ): n(n) {
-      std::fill(data, data + SEG_TREE_DATA_SIZE, IDENTITY);
-    }
-    
-    void update( int k, LL a ) {
-      k += n - 1;
-      data[k] = a;
-      while ( k > 0 ) {
-        k = ( k - 1 ) / 2;
-        data[k] = std::max(data[k * 2 + 1], data[k * 2 + 2]);
-      }
-    }
-    
-    // [a, b)の最大値
-    LL query( int a, int b, int k, int l, int r ) {
-      if ( r <= a || b <= l ) {
-        return IDENTITY;
-      }
-      if ( a <= l && r <= b ) {
-        return data[k];
-      } else {
-        int mid = ( l + r ) / 2;
-        LL vl = query(a, b, k * 2 + 1, l, mid);
-        LL vr = query(a, b, k * 2 + 2, mid, r);
-        return std::max(vl, vr);
-      }
-      
-    }
-    
-    int n;
-    LL data[SEG_TREE_DATA_SIZE];
-  };
 }
 
 // @snippet<sh19910711/contest:solution/solver-area.cpp>
@@ -189,28 +197,33 @@ namespace solution {
   public:
     void solve() {
       normalize();
-      
-      /*
-         cout << "C:" << endl;
-         for ( int i = 0; i < n; ++ i ) {
-         cout << i << ": " << C[i] << endl;
-         }
-         cout << "D:" << endl;
-         for ( int i = 0; i < n; ++ i ) {
-         cout << i << ": " << D[i] << endl;
-         }
-         */
-      
-      find_nice_orders();
-      
-      
-      SegmentTree tree(n);
-      for ( int i = 1; i <= n; ++ i ) {
-        tree.update(i, A[i]);
+      generate_tree();
+
+      cout << "C:" << endl;
+      for ( int i = 0; i < n; ++ i ) {
+        cout << i << ": " << C[i] << endl;
       }
-      
-      for ( int i = 1; i + 1 <= n; ++ i ) {
-        cout << i << ": " << tree.query(i, i + 1, 0, 0, n) << endl;
+      cout << "D:" << endl;
+      for ( int i = 0; i < n; ++ i ) {
+        cout << i << ": " << D[i] << ": A = " << A[D[i].second] << endl;
+      }
+
+      // [l, r)
+      // cout << tree_a.query(0, 1, 0, 0, 2 * n - 1);
+      //
+      cout << "query_test: " << tree_b.query(0, n, 0, 0, 2 * n - 1) << endl;
+
+      find_nice_orders();
+    }
+
+    void generate_tree() {
+      tree_a.init(n);
+      tree_b.init(n);
+      for ( int i = 0; i < n; ++ i ) {
+        tree_pos_a[C[i].second] = i;
+        tree_a.update(i, Entry(A[C[i].second], C[i].second));
+        tree_pos_b[D[i].second] = i;
+        tree_b.update(i, Entry(B[D[i].second], D[i].second));
       }
     }
     
@@ -220,10 +233,41 @@ namespace solution {
         D[i] = II(B[i], i);
       }
       sort(C, C + n, std::greater<II>());
-      sort(D, D + n, std::greater<II>());
+      sort(D, D + n, std::less<II>());
     }
     
     void find_nice_orders() {
+      int need_bad_num = p - k;
+      int bad_num = 0;
+      printf("@find_nice_orders: need_bad_num = %d\n", need_bad_num);
+
+      for ( int i = 0; i < n && nice_num < p; ++ i ) {
+        Entry nice_entry = tree_a.query(0, n, 0, 0, 2 * n - 1);
+        int good_order_index = nice_entry.second;
+        int a = A[good_order_index];
+        int b = B[good_order_index];
+
+        cout << "nice_entry: " << nice_entry << " : " << a << ", " << b << endl;
+        bad_num ++;
+        nice[nice_num ++] = good_order_index;
+        tree_a.update(tree_pos_a[good_order_index], IDENTITY_ENTRY);
+        tree_b.update(tree_pos_b[good_order_index], IDENTITY_ENTRY);
+
+        while ( nice_num < p && bad_num < need_bad_num ) {
+          cout << "hello" << endl;
+          int left_cand = std::lower_bound(D, D + n, II(b, -1)) - D;
+          cout << "left_cand = " << left_cand << endl;
+          Entry bad_but_nice_entry = tree_b.query(0, left_cand, 0, 0, 2 * n - 1);
+          if ( bad_but_nice_entry == IDENTITY_ENTRY )
+            break;
+          cout << "max_orderのB以下でBの最大値 = " << bad_but_nice_entry << endl;
+          int order_index = bad_but_nice_entry.second;
+          nice[nice_num ++] = order_index;
+          bad_num ++;
+          tree_a.update(tree_pos_a[order_index], IDENTITY_ENTRY);
+          tree_b.update(tree_pos_b[order_index], IDENTITY_ENTRY);
+        }
+      }
     }
     
   private:
@@ -252,7 +296,7 @@ namespace solution {
     bool input() {
       if ( ! ( cin >> n >> p >> k ) )
         return false;
-      for ( int i = 1; i <= n; ++ i ) {
+      for ( int i = 0; i < n; ++ i ) {
         cin >> A[i] >> B[i];
       }
       return true;
@@ -260,7 +304,7 @@ namespace solution {
     
     void output() {
       for ( int i = 0; i < nice_num; ++ i ) {
-        cout << nice[i] << " ";
+        cout << nice[i] + 1 << " ";
       }
       cout << endl;
     }
---
+++ 332C/unit_tests.cpp
@@ -0,0 +1,19 @@
+
+// @snippet<sh19910711/contest:unit-tests.cpp>
+#define __MY_UNIT_TEST__
+#include "322C.cpp"
+#include <gtest/gtest.h>
+
+/*
+ *  # Example
+ *
+ *  int add( int x, int y ) {
+ *    return x + y;
+ *  }
+ *
+ *  TEST(AddTest, Test1) {
+ *    ASSERT_EQ(2, add(1, 1));
+ *  }
+ */
+
+
