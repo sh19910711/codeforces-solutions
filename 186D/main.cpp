349b00d5a2cf6f059c0814a2f6c1f11bc8346b6f
Hiroyuki Sano <sh19910711@gmail.com>
2013-07-26 17:18:41 +0900

    Codeforces 186D: Accepted

---
+++ 186D/main.cpp
@@ -0,0 +1,143 @@
+
+// @snippet<sh19910711/contest:headers.cpp>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <complex>
+#include <functional>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+// @snippet<sh19910711/contest:solution/interface.cpp>
+namespace solution {
+  class SolutionInterface {
+  public:
+    virtual int run() = 0;
+    
+  protected:
+    virtual void pre_calc() {}
+    virtual bool action() = 0;
+    virtual void init() {};
+    virtual bool input() { return false; };
+    virtual void output() {};
+    
+    SolutionInterface() {}
+    
+  private:
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution-base.cpp>
+namespace solution {
+  class SolutionBase: public SolutionInterface {
+  public:
+    virtual int run() {
+      pre_calc();
+      while ( action() );
+      return 0;
+    }
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/typedef.cpp>
+namespace solution {
+  typedef std::istringstream ISS;
+  typedef std::ostringstream OSS;
+  typedef std::vector<std::string> VS;
+  typedef long long LL;
+  typedef int INT;
+  typedef std::vector<INT> VI;
+  typedef std::vector<VI> VVI;
+  typedef std::pair<INT,INT> II;
+  typedef std::vector<II> VII;
+}
+
+// @snippet<sh19910711/contest:solution/namespace-area.cpp>
+namespace solution {
+  // namespaces, types
+  using namespace std;
+  typedef double Double;
+}
+
+// @snippet<sh19910711/contest:solution/variables-area.cpp>
+namespace solution {
+  // constant vars
+  
+  // storages
+  Double S;
+  Double a, b, c;
+
+  Double x, y, z;
+}
+
+// @snippet<sh19910711/contest:solution/solver-area.cpp>
+namespace solution {
+  class Solver {
+  public:
+    void solve() {
+      Double s = a + b + c;
+      if ( s == 0 ) {
+        x = 0;
+        y = 0;
+        z = 0;
+      } else {
+        x = a * S / s;
+        y = b * S / s;
+        z = c * S / s;
+      }
+    }
+    
+  private:
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution.cpp>
+namespace solution {
+  class Solution: public SolutionBase {
+  public:
+  protected:
+    virtual bool action() {
+      init();
+      if ( ! input() )
+        return false;
+      solver.solve();
+      output();
+      return true;
+    }
+
+    bool input() {
+      return cin >> S >> a >> b >> c;
+    }
+
+    void output() {
+      printf("%.12f %.12f %.12f\n", x, y, z);
+    }
+    
+  private:
+    Solver solver;
+    
+  };
+}
+
+// @snippet<sh19910711/contest:main.cpp>
+#ifndef __MY_UNIT_TEST__
+int main() {
+  return solution::Solution().run();
+}
+#endif
+
