b73fa4808b43df257ab71221df8281dff701d071
Hiroyuki Sano <sh19910711@gmail.com>
2013-07-28 05:02:05 +0900

    Codeforces 143D: Accepted

---
+++ 143D/main.cpp
@@ -0,0 +1,153 @@
+
+// @snippet<sh19910711/contest:headers.cpp>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <complex>
+#include <functional>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+// @snippet<sh19910711/contest:solution/interface.cpp>
+namespace solution {
+  class SolutionInterface {
+  public:
+    virtual int run() = 0;
+    
+  protected:
+    virtual void pre_calc() {}
+    virtual bool action() = 0;
+    virtual void init() {};
+    virtual bool input() { return false; };
+    virtual void output() {};
+    
+    SolutionInterface() {}
+    
+  private:
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution-base.cpp>
+namespace solution {
+  class SolutionBase: public SolutionInterface {
+  public:
+    virtual int run() {
+      pre_calc();
+      while ( action() );
+      return 0;
+    }
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/typedef.cpp>
+namespace solution {
+  typedef std::istringstream ISS;
+  typedef std::ostringstream OSS;
+  typedef std::vector<std::string> VS;
+  typedef long long LL;
+  typedef int INT;
+  typedef std::vector<INT> VI;
+  typedef std::vector<VI> VVI;
+  typedef std::pair<INT,INT> II;
+  typedef std::vector<II> VII;
+}
+
+// @snippet<sh19910711/contest:solution/namespace-area.cpp>
+namespace solution {
+  // namespaces, types
+  using namespace std;
+  
+}
+
+// @snippet<sh19910711/contest:solution/variables-area.cpp>
+namespace solution {
+  // constant vars
+  
+  // storages
+  int n, m;
+  int result;
+}
+
+// @snippet<sh19910711/contest:solution/solver-area.cpp>
+namespace solution {
+  class Solver {
+  public:
+    void solve() {
+      result = 0;
+      if ( n == 1 ) {
+        result = max(result, m);
+      }
+      if ( m == 1 ) {
+        result = max(result, n);
+      }
+      if ( n == 2 ) {
+        if ( m % 4 == 3 ) {
+          result = max(result, m + 1);
+        } else {
+          result = max(result, m + m % 4);
+        }
+      }
+      if ( m == 2 ) {
+        if ( n % 4 == 3 ) {
+          result = max(result, n + 1);
+        } else {
+          result = max(result, n + n % 4);
+        }
+      }
+      result = max(result, (int)( ( n * m + 1.0 ) / 2 ));
+    }
+    
+  private:
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution.cpp>
+namespace solution {
+  class Solution: public SolutionBase {
+  public:
+  protected:
+    virtual bool action() {
+      init();
+      if ( ! input() )
+        return false;
+      solver.solve();
+      output();
+      return true;
+    }
+
+    bool input() {
+      return cin >> n >> m;
+    }
+
+    void output() {
+      cout << result << endl;
+    }
+    
+  private:
+    Solver solver;
+    
+  };
+}
+
+// @snippet<sh19910711/contest:main.cpp>
+#ifndef __MY_UNIT_TEST__
+int main() {
+  return solution::Solution().run();
+}
+#endif
+
