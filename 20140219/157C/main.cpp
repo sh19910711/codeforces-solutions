e625a62d858cbec86a3077a5f94d01cd4f26ba2e
Hiroyuki Sano <sh19910711@gmail.com>
2014-02-19 15:50:27 +0900

    Codeforces 157C: Accepted

---
+++ 20140219/157C/main.cpp
@@ -0,0 +1,126 @@
+
+// @snippet<sh19910711/contest-base-simple:header.cpp>
+#define __THIS_IS_CPP11__
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <cstdio>
+#include <cmath>
+
+#ifdef __THIS_IS_CPP11__
+#include <memory>
+#include <array>
+#endif
+
+// @snippet<sh19910711/contest-base-simple:solution/template-typedef.cpp>
+namespace solution {
+  typedef long long Int;
+  typedef long double Double;
+  typedef std::string String;
+}
+
+// @snippet<sh19910711/contest-base-simple:solution/template-constants.cpp>
+namespace solution {
+}
+
+// @snippet<sh19910711/contest-base-simple:solution/template-storages.cpp>
+namespace solution {
+  struct Input {
+    String S;
+    String U;
+  };
+  struct Output {
+    Int result;
+  };
+}
+
+// @snippet<sh19910711/contest-base-simple:solution/solution.cpp>
+namespace solution {
+  struct Solution {
+    //
+    // Implement here
+    //
+    
+    const Int INF = std::numeric_limits<Int>::max();
+
+    void init() {
+    }
+    
+    void solve() {
+      out->result = count(std::string(2000, ' ') + in->S + std::string(2000, ' '), in->U);
+    }
+
+    Int count( const String& s, const String& u ) {
+      Int res = INF;
+      Int N = s.size();
+      Int M = u.size();
+      for ( auto i = 0; i + M - 1 < N; ++ i ) {
+        Int cnt = 0;
+        for ( auto j = 0; j < M; ++ j ) {
+          if ( s[i + j] != u[j] )
+            cnt ++;
+        }
+        res = std::min(res, cnt);
+      }
+      return res;
+    }
+    
+    bool input() {
+      return std::cin >> in->S >> in->U;
+    }
+    
+    void output() {
+      std::cout << out->result << std::endl;
+    }
+    
+    //
+    //
+    //
+    Solution( Input* in, Output* out ): in(in), out(out) { this->init(); }
+    
+    int run() {
+      for (;;) {
+        this->before_action(-1);
+        if ( ! this->action() ) { this->after_action(-1); break; }
+        this->after_action(-1);
+      };
+      return 0;
+    }
+    
+    bool action() {
+      if ( ! this->input() ) return false;
+      this->solve();
+      this->output();
+      return true;
+    }
+    
+    void before_action( const int& t_id ) {}
+    
+    void after_action( const int& t_id ) {}
+    
+    Input* in;
+    Output* out;
+  };
+}
+
+// @snippet<sh19910711/contest-base-simple:main_func.cpp>
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  using namespace solution;
+  static Input in;
+  static Output out;
+  static Solution sol(&in, &out);
+  return sol.run();
+}
+
+// @snippet<sh19910711/contest-base-simple:main.cpp>
+
