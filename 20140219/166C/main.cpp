c42559891e050bc070693daa25694ca163bf103a
Hiroyuki Sano <sh19910711@gmail.com>
2014-02-19 19:06:45 +0900

    Codeforces 166C: Accepted

---
+++ 20140219/166C/main.cpp
@@ -0,0 +1,153 @@
+
+// @snippet<sh19910711/contest-base-simple:header.cpp>
+#define __THIS_IS_CPP11__
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <cstdio>
+#include <cmath>
+
+#ifdef __THIS_IS_CPP11__
+#include <memory>
+#include <array>
+#endif
+
+// @snippet<sh19910711/contest-base-simple:solution/template-typedef.cpp>
+namespace solution {
+  typedef long long Int;
+  typedef long double Double;
+  typedef std::string String;
+}
+
+// @snippet<sh19910711/contest-base-simple:solution/template-constants.cpp>
+namespace solution {
+  const Int SIZE = 500 + 11;
+}
+
+// @snippet<sh19910711/contest-base-simple:solution/template-storages.cpp>
+namespace solution {
+  struct Input {
+    Int N;
+    Int X;
+    Int A[SIZE];
+  };
+  struct Output {
+    Int result;
+  };
+}
+
+// @snippet<sh19910711/contest-base-simple:solution/solution.cpp>
+namespace solution {
+  struct Solution {
+    //
+    // Implement here
+    //
+    
+    typedef std::deque<Int> IntDeque;
+
+    Int N;
+    IntDeque A;
+
+    void init() {
+    }
+    
+    void solve() {
+      N = in->N;
+      A.clear();
+      for ( auto i = 0; i < N; ++ i ) {
+        A.push_back(in->A[i]);
+      }
+      out->result = find();
+    }
+
+    bool has() {
+      for ( auto i = 0; i < in->N; ++ i ) {
+        if ( A[i] == in->X )
+          return true;
+      }
+      return false;
+    }
+
+    Int find() {
+      Int res = 0;
+      if ( ! has() ) {
+        A.push_back(in->X);
+        res ++;
+      }
+      std::sort(std::begin(A), std::end(A));
+      while ( A[(A.size() * 2 - 1 ) / 4] > in->X ) {
+        A.push_front(1);
+        res ++;
+      }
+      while ( A[(A.size() * 2 - 1 ) / 4] < in->X ) {
+        A.push_back(1);
+        res ++;
+      }
+      // std::cout << "A: "; for ( auto v : A ) std::cout << v << ", "; std::cout << std::endl;
+      return res;
+    }
+    
+    bool input() {
+      if ( ! ( std::cin >> in->N >> in->X ) )
+        return false;
+      for ( auto i = 0; i < in->N; ++ i )
+        if ( ! ( std::cin >> in->A[i] ) )
+          return false;
+      return true;
+    }
+    
+    void output() {
+      std::cout << out->result << std::endl;
+    }
+    
+    //
+    //
+    //
+    Solution( Input* in, Output* out ): in(in), out(out) { this->init(); }
+    
+    int run() {
+      for (;;) {
+        this->before_action(-1);
+        if ( ! this->action() ) { this->after_action(-1); break; }
+        this->after_action(-1);
+      };
+      return 0;
+    }
+    
+    bool action() {
+      if ( ! this->input() ) return false;
+      this->solve();
+      this->output();
+      return true;
+    }
+    
+    void before_action( const int& t_id ) {}
+    
+    void after_action( const int& t_id ) {}
+    
+    Input* in;
+    Output* out;
+  };
+}
+
+// @snippet<sh19910711/contest-base-simple:main_func.cpp>
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  using namespace solution;
+  static Input in;
+  static Output out;
+  static Solution sol(&in, &out);
+  return sol.run();
+}
+
+// @snippet<sh19910711/contest-base-simple:main.cpp>
+
