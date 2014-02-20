b1a841484f289c0087edcae63a08cdfe6c1e309e
Hiroyuki Sano <sh19910711@gmail.com>
2014-02-21 02:35:08 +0900

    Codeforces 394D: Wrong Answer

---
+++ codeforces_round_231/D/main.cpp
@@ -0,0 +1,145 @@
+
+// @snippet<sh19910711/contest-base-simple:header.cpp>
+#define __THIS_IS_CPP11__
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <cstdio>
+#include <cmath>
+
+#ifdef __THIS_IS_CPP11__
+#include <memory>
+#include <array>
+#endif
+
+// @snippet<sh19910711/contest-base-simple:solution/template-typedef.cpp>
+namespace solution {
+  typedef long long Int;
+  typedef long double Double;
+  typedef std::string String;
+}
+
+// @snippet<sh19910711/contest-base-simple:solution/template-constants.cpp>
+namespace solution {
+  const Int SIZE = 1000 + 11;
+}
+
+// @snippet<sh19910711/contest-base-simple:solution/template-storages.cpp>
+namespace solution {
+  struct Input {
+    Int N;
+    Int A[SIZE];
+  };
+  struct Output {
+    Int bun;
+    Int start;
+    Int diff;
+  };
+}
+
+// @snippet<sh19910711/contest-base-simple:solution/solution.cpp>
+namespace solution {
+  struct Solution {
+    //
+    // Implement here
+    //
+    
+    void init() {
+    }
+    
+    Int N;
+    Int A[SIZE];
+
+    void solve() {
+      N = in->N;
+      std::copy(in->A, in->A + N, A);
+      std::sort(A, A + N);
+      Int sum = 0;
+      for ( auto i = 0; i < N; ++ i ) {
+        sum += std::abs(A[i]);
+      }
+
+      if ( N == 2 ) {
+        out->bun = 0;
+        out->start = A[0];
+        out->diff = A[1] - A[0];
+        return;
+      }
+
+      Int lb = 0;
+      Int ub = 1000000000LL;
+      while ( ub - lb > 1 ) {
+        Int mid = ( lb + ub ) * 0.5;
+        if ( N * mid <= sum ) {
+          lb = mid;
+        } else {
+          ub = mid;
+        }
+      }
+      std::cout << "lb = " << lb << std::endl;
+    }
+    
+    bool input() {
+      if ( ! ( std::cin >> in->N ) )
+        return false;
+      for ( auto i = 0; i < in->N; ++ i )
+        if ( ! ( std::cin >> in->A[i] ) )
+          return false;
+      return true;
+    }
+    
+    void output() {
+      std::cout << out->bun << " " << out->start << " " << out->diff << std::endl;
+    }
+    
+    //
+    //
+    //
+    Solution( Input* in, Output* out ): in(in), out(out) { this->init(); }
+    
+    int run() {
+      for (;;) {
+        this->before_action(-1);
+        if ( ! this->action() ) { this->after_action(-1); break; }
+        this->after_action(-1);
+      };
+      return 0;
+    }
+    
+    bool action() {
+      if ( ! this->input() ) return false;
+      this->solve();
+      this->output();
+      return true;
+    }
+    
+    void before_action( const int& t_id ) {}
+    
+    void after_action( const int& t_id ) {}
+    
+    Input* in;
+    Output* out;
+  };
+}
+
+// @snippet<sh19910711/contest-base-simple:main_func.cpp>
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  using namespace solution;
+  static Input in;
+  static Output out;
+  static Solution sol(&in, &out);
+  return sol.run();
+}
+
+// @snippet<sh19910711/contest-base-simple:main.cpp>
+
